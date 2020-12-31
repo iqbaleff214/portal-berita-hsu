355a74bb317d9ec833b75512ce1c2a1c917665b2
M. Iqbal Effendi <iqbaleff214@gmail.com>
2020-12-31 17:08:46 +0800

    Nambah email admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\News;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +19,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(3)->create();
+        User::create([
+            'name' => 'Marjuni',
+            'email' => 'admin@admin.io',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin'), // password
+            'remember_token' => Str::random(10),
+        ]);
         Category::create([
             'category' => 'Berita'
         ]);
